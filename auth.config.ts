115ad297d8c0f09cea39dd12d33b879cb212aeb5
Benjamin Saks <saksben2@gmail.com>
2024-05-29 00:41:10 -0700

    add authentication

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  // Specifies the login page path
+  pages: {
+    signIn: '/login',
+  },
+  // Prevents users from accessing pages unless they are loggeed in
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
